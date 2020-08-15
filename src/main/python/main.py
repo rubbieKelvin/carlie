674f0c6941b1326a86a6a03d4cf76c1e879b7161
rubbiekelvin <rubbiekelvinvoltsman@gmail.com>
2020-08-16 00:36:25 +0100

    initial commit

---
+++ src/main/python/main.py
@@ -0,0 +1,12 @@
+from fbs_runtime.application_context.PySide2 import ApplicationContext
+from PySide2.QtWidgets import QMainWindow
+
+import sys
+
+if __name__ == '__main__':
+    appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
+    window = QMainWindow()
+    window.resize(250, 150)
+    window.show()
+    exit_code = appctxt.app.exec_()      # 2. Invoke appctxt.app.exec_()
+    sys.exit(exit_code)
