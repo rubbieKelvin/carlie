4139e1e432c2925756354187e4d5439714b749b0
rubbiekelvin <rubbiekelvinvoltsman@gmail.com>
2020-09-05 16:53:49 +0100

    sudo rm *

--- src/main/python/carlie_ai/preprocessors.py
+++
@@ -1,5 +0,0 @@
-from chatterbot.conversation import Statement
-
-def lower(statement:Statement) -> Statement:
-    statement.text = statement.text.lower()
-    return statement
--- src/main/python/plugins/__init__.py
+++
@@ -1,4 +0,0 @@
-from PySide2 import QtCore
-
-def pluginlabel(className:QtCore.QObject, name:str) -> tuple:
-    return className, "Carlie.Plugins", 1, 0, name
